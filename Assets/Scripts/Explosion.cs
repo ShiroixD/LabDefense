57d70c2dbeff02878de598a2e7bbf65c57f021b3
Shiroi <fabian_frontczak@wp.pl>
2020-05-01 22:30:45 +0200

    Dodano prostszą wersję wolumerycznej eksplozji

---
+++ Assets/Scripts/Explosion.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour
+{
+    public int loopduration;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        float r = Mathf.Sin((Time.time / loopduration) * (2 * Mathf.PI)) * 0.5f + 0.25f;
+        float g = Mathf.Sin((Time.time / loopduration + 0.33333333f) * 2 * Mathf.PI) * 0.5f + 0.25f;
+        float b = Mathf.Sin((Time.time / loopduration + 0.66666667f) * 2 * Mathf.PI) * 0.5f + 0.25f;
+        float correction = 1 / (r + g + b);
+        r *= correction;
+        g *= correction;
+        b *= correction;
+        GetComponent<Renderer>().material.SetVector("_ChannelFactor", new Vector4(r, g, b, 0));
+    }
+}
