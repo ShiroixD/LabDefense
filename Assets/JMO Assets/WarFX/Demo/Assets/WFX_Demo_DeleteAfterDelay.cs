76f61ea8e64651cbacb890046e065ed2ccf5ae4e
Shiroi <fabian_frontczak@wp.pl>
2020-06-06 01:40:07 +0200

    Dodano raycasta strzał i efekty graficzne particle

---
+++ Assets/JMO Assets/WarFX/Demo/Assets/WFX_Demo_DeleteAfterDelay.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using System.Collections;
+
+public class WFX_Demo_DeleteAfterDelay : MonoBehaviour
+{
+	public float duration = 1.0f;
+	public float currentTime = 0;
+
+	void Start()
+	{
+		currentTime = duration;
+	}
+
+	void Update ()
+	{
+		if (currentTime > 0)
+		{
+			currentTime -= Time.deltaTime;
+		}
+
+		if (currentTime < 0f)
+		{
+			gameObject.SetActive(false);
+		}
+	}
+}
---
+++ Assets/Scripts/Target.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    public float health = 50f;
+    public float dieDelay = 3f;
+
+    public void TakeDamage(float amount)
+    {
+        health -= amount;
+        if (health <= 0f)
+        {
+            Die();
+        }
+    }
+
+    IEnumerator DelayedDie(float time)
+    {
+        foreach (Rigidbody rg in GetComponentsInChildren<Rigidbody>())
+        {
+            rg.isKinematic = false;
+        }
+        yield return new WaitForSeconds(time);
+        Destroy(gameObject);
+    }
+
+    void Die()
+    {
+        StartCoroutine(DelayedDie(dieDelay));
+    }
+}
