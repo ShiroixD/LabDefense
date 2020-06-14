dfcbfa315abd4d3a56cf7a12e13207514a760c46
Shiroi <fabian_frontczak@wp.pl>
2020-06-14 20:17:31 +0200

    Dodano zadawanie damagu graczowi

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -1,18 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class Target : MonoBehaviour
 {
     public float health = 50f;
+    public float damage = 10f;
+    public float attackDelay = 0.5f;
     public float dieDelay = 3f;
     public Destructible destructible;
     public float lookRadius = 10f;
 
     private Transform target;
     private NavMeshAgent agent;
+    private bool isAttacking;
 
     void Start()
     {
@@ -30,7 +35,28 @@ public class Target : MonoBehaviour
             {
                 agent.SetDestination(target.position);
             }
+
+            if (distance <= agent.stoppingDistance)
+            {
+                FaceTarget();
+                if (!isAttacking)
+                {
+                    isAttacking = true;
+                    StartCoroutine(Attack(attackDelay));
+                }
+            }
+            else
+            {
+                isAttacking = false;
+            }
         }
+    }
+
+    void FaceTarget()
+    {
+        Vector3 direction = (target.position - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
     }
 
     void OnDrawGizmosSelected()
@@ -44,9 +70,26 @@ public class Target : MonoBehaviour
         health -= amount;
         if (health <= 0f)
         {
+            isAttacking = false;
             if (destructible != null)
+            {
                 destructible.Destroy();
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
         }
     }
 
+
+    IEnumerator Attack(float time)
+    {
+        Player player = target.GetComponent<Player>();
+        while (isAttacking)
+        {
+            yield return new WaitForSeconds(time);
+            player.TakeDamage((int)damage);
+        }
+    }
 }
