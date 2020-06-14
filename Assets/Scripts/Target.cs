2457763d627b7b94003b63319dfd5bd60371e488
Shiroi <fabian_frontczak@wp.pl>
2020-06-14 19:41:21 +0200

    Nowa scena, pasek życia

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -2,12 +2,42 @@
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Target : MonoBehaviour
 {
     public float health = 50f;
     public float dieDelay = 3f;
     public Destructible destructible;
+    public float lookRadius = 10f;
+
+    private Transform target;
+    private NavMeshAgent agent;
+
+    void Start()
+    {
+        target = PlayerManager.instance.player.transform;
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        if (agent != null)
+        {
+            float distance = Vector3.Distance(target.position, transform.position);
+
+            if (distance <= lookRadius)
+            {
+                agent.SetDestination(target.position);
+            }
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, lookRadius);
+    }
 
     public void TakeDamage(float amount)
     {
@@ -18,4 +48,5 @@ public class Target : MonoBehaviour
                 destructible.Destroy();
         }
     }
+
 }
