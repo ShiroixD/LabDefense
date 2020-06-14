18885ded7841cc3aadb5d17970b3cf37b9e83e3b
Shiroi <fabian_frontczak@wp.pl>
2020-06-14 19:50:53 +0200

    Dodano health bar gracza

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private Healthbar _healthBar;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void TakeDamage(int amount)
+    {
+        _healthBar.TakeDamage(amount);
+    }
+
+    public void GainHealth(int amount)
+    {
+        _healthBar.GainHealth(amount);
+    }
+
+    public void StartRegenerating()
+    {
+        _healthBar.regenerateHealth = true;
+    }
+
+    public void StopRegenerating()
+    {
+        _healthBar.regenerateHealth = false;
+    }
+}
